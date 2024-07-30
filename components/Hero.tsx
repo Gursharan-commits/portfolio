e4c4e4c60ed0ef26912af5eb5367b6c11224ab50
gras4747 <gursharan161@gmail.com>
2024-07-30 12:44:35 +0530

    front page bento grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -20,10 +20,9 @@ const Hero = () => {
 
             <div className='flex justify-center relative my-20 z-10'>
                 <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
-                    <h2 className='uppercase tracking-widest text-xs text-center text-blue-100 max-w-80'>dynamic web magic with next.js</h2>
                     <TextGenerateEffect
                         className='text-center text-[40px] md: text-5xl lg:text-6xl'
-                        words='Transforming coepts into seamless experiences'
+                        words='Transforming concepts into seamless experiences'
                     />
                     <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
                         Hi I&apos;m Gursharan Singh, a full stack developer and data analyst based in India
